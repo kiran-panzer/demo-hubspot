eba0f3a6f58d9ce59e6d16aac1b68500fbcb92ca
Kiran-Panzer <kiran.kumar@panzertechnologies.net>
2013-09-02 13:19:29 +0530

    hubspot app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubspot::Application.config.secret_token = '8f496e39da5d2bfd6dbf88d4e98af1d9ea6147c5dbc421080535dd128b7fd6b9ebd26e18fcd6fa4e4cb22b2c5169699caee311920be56373badb0dda6edfb3c3'
